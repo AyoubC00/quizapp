31958853ec67d59cc14d3b8c63e8d6f041694ebf
ouaissa <abdououaissa37@gmail.com>
2023-12-27 15:34:00 +0100

    Implement useGetQuestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Timer from "./components/Timer/Timer"
+import useGetQuesitions from "./hooks/useGetQuestions"
 
 function App() {
-
+  const [question, getQuestion] = useGetQuesitions([{n: 1}, {n: 2}, {n: 3}, {n: 4}]);
   return (
     <>
-      <Timer hours={ 3 } minutes={ 0 } seconds={ 0 }/>
+      <Timer hours={ 3 } minutes={ 0 } seconds={ 0 } />
     </>
   )
 }
--- src/hooks/useGetQuestions.js
+++ src/hooks/useGetQuestions.js
@@ -1,8 +1,20 @@
 import { useState } from "react"
 
-const useGetQuesitions = () =>
+const useGetQuesitions = (questions) =>
 {
-    
+    const [position, setPosition] = useState(0);
+    const getQuestion = direction =>
+    {
+        if (direction === 1)
+        {
+            setPosition(prev => prev + 1);
+        }
+        else if (direction === -1)
+        {
+            setPosition(prev => prev - 1);
+        }
+    }   
+    return [questions[position], getQuestion];
 }
 
 export default useGetQuesitions
